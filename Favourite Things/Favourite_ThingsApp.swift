4b2f907d3c5f45ff1786518c6f3c3c4970714d05
Bradley Kang <bkang@lcs.on.ca>
2021-02-04 09:48:12 -0500

    Initial Commit

---
+++ Favourite Things/Favourite_ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite_ThingsApp.swift
+//  Favourite Things
+//
+//  Created by Brad Kang on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct Favourite_ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
