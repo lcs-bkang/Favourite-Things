2e90bb1d95a1c27dd70094443f17dcf631011aec
Bradley Kang <bkang@lcs.on.ca>
2021-02-09 15:08:07 -0500

    Created and finished the thing detail view

--- Views/ThingDetail.swift
+++ Views/ThingDetail.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ThingDetail: View {
+    var heroImage: String
+    var details: String
+    var title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+        
+            Image(heroImage)
+                .resizable()
+                .scaledToFit()
+            
+            Text(details)
+                .padding()
+            
+        }
+        .navigationTitle(title)
+
     }
 }
 
 struct ThingDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ThingDetail()
+        ThingDetail(heroImage: favouriteThings[0].heroImage, details: favouriteThings[0].details, title: favouriteThings[0].title)
     }
 }
