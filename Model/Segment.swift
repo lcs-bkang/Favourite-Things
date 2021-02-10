644c76e5b6020a0629eb7e3a43cd35b963b32dd5
Bradley Kang <bkang@lcs.on.ca>
2021-02-10 11:56:19 -0500

    Created and finished segment model

---
+++ Model/Segment.swift
@@ -0,0 +1,15 @@
+//
+//  Segment.swift
+//  Favourite Things
+//
+//  Created by Brad Kang on 2021-02-10.
+//
+
+import Foundation
+
+struct Segments: Identifiable {
+    
+    var id = UUID()
+    var image: String
+    var text: String
+}
