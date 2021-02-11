e67d9613c54d6abf96ef8b055c424afdf90ef52d
Bradley Kang <bkang@lcs.on.ca>
2021-02-11 08:44:27 -0500

    Added segment to soccer

--- Model/Thing.swift
+++ Model/Thing.swift
@@ -27,7 +27,14 @@ let favouriteThings = [
                     text: "Soccer has been my first sport and has made me who I am physically.  Also, I enjoy trying new tricks and competing against other teams.  One of the most important parts of soccer for me is that many of my friends from my soccer team are very close, especially during tournaments where we all hang out in a hotel."),
             Segment(image: "TorontoFC",
                     text: """
-                    The first and only professional soccer game that I attended was a match between Toronto FC and Minnesota United.  As the first professional match I've ever been to, it was the greatest one to have chosen to watch.  It had everything: comebacks, a lot of goals, crazy skills, penalties, and even red cards.  There's no other match that I'd have watched instead for the first professional match I'd ever watched.  Even if this game was in the lowly MLS.
+                    The first and only professional soccer game that I attended was a match between Toronto FC and Minnesota United.  As the first professional match I've ever been to, it was the greatest one to have chosen to watch.  It had everything: comebacks, a lot of goals, crazy skills, penalties, and even red cards.
+
+                    There's no other match that I'd have watched instead for the first professional match I'd ever watched.  Even if this game was in the lowly MLS.
+                """),
+            Segment(image: "Messi", text: """
+                    Of course, I wouldn't be a soccer fan without having an opinion on the Lionel Messi vs Cristiano Ronaldo debate.  For myself, I believe that Lionel Messi is the superior player.  Of course, this is a highly debated topic throughout their careers, and I highly respect them both.
+
+                    I just feel that Messi is more elegant in his play, something I prefer over Ronaldo's brute force dance.
                 """)
                 
             ]),
